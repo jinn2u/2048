aa28d433069e130d39e61acc6ef30e2573376a84
jinn2u <bsybear623@gmail.com>
2021-07-31 00:48:04 +0900

    feat: 타일배열 lodash로 간편화하기

--- src/component/Game.js
+++ src/component/Game.js
@@ -1,39 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import times from 'lodash/times'
+import { MAX_POS } from '../constant'
 
 export default function Game() {
+  const [tileList, setTileList] = useState([])
   return (
     <div className="game-container">
       <div className="grid-container">
-        <div className="grid-row">
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-        </div>
-        <div className="grid-row">
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-        </div>
-        <div className="grid-row">
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-        </div>
-        <div className="grid-row">
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-          <div className="grid-cell"></div>
-        </div>
-      </div>
-      <div className="tile-container">
-        <div className="tile tile-2 tile-position-3-4 tile-new">
-          <div className="tile-inner">2</div></div>
-        <div className="tile tile-2 tile-position-4-3 tile-new">
-          <div className="tile-inner">2</div>
+        {times(MAX_POS, () => (
+          <div className="grid-row">
+            {times(MAX_POS, () => (
+              <div className="grid-cell"></div>
+            ))}
+          </div>
+        ))}
+        <div className="tile-container">
+          {tileList.map(item => (
+            <div className={`tile tile-${item.value} tile-position-${item.x}-${item.y} tile-new`}>
+              <div className="tile-inner">{item.value}</div>
+            </div>
+          ))}
+
         </div>
       </div>
     </div>
