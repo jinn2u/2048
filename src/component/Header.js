a3566fc87c9e21eae428157c4f1aa010bac961cd
jinn2u <bsybear623@gmail.com>
2021-07-31 20:31:51 +0900

    feat: [Header] score 표시하기

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function App() {
+export default function Header({score, bestScore}) {
   return (
     <div className="heading">
       <h1 className="title">2048</h1>
       <div className="scores-container">
-        <div className="score-container">0</div>
+        <div className="score-container">{score}</div>
         <div className="best-container">0</div>
       </div>
     </div>
