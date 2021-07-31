806d82c420f3f6c85230f4ebe1359db9fd18b3e5
jinn2u <bsybear623@gmail.com>
2021-07-31 11:37:31 +0900

    feat: 초기 타일의 좌표 랜덤으로 2개 생성하기

--- src/component/Game.js
+++ src/component/Game.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import times from 'lodash/times'
 import { MAX_POS } from '../constant'
+import { getInitialTileList } from '../util/tile'
 
 export default function Game() {
-  const [tileList, setTileList] = useState([])
+  const [tileList, setTileList] = useState(getInitialTileList)
   return (
     <div className="game-container">
       <div className="grid-container">
@@ -14,15 +15,14 @@ export default function Game() {
             ))}
           </div>
         ))}
-        <div className="tile-container">
+      </div>
+      <div className="tile-container">
           {tileList.map(item => (
             <div className={`tile tile-${item.value} tile-position-${item.x}-${item.y} tile-new`}>
               <div className="tile-inner">{item.value}</div>
             </div>
           ))}
-
         </div>
-      </div>
     </div>
   )
 }
---
+++ src/util/number.js
@@ -0,0 +1,3 @@
+export function getRandomInteger(from, to){
+  return Math.floor(Math.random() * from * to)
+}
